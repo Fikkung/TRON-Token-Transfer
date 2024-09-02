60b2f82310a100ee96f0f221206e224f41b51bf7
Fikkung <125670574+Fikkung@users.noreply.github.com>
2024-09-02 11:42:21 +0700

    test2

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.post('/send-token', async (req, res) => {
     }
 });
 
-app.listen(5500, () => {
-    console.log('Server is running on port 5500');
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
